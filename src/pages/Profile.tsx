9fca38b8f3dc537266a8dd81a13f60e78c9b6ff6
Birhane <birhane.kahsay99@gmail.com>
2024-10-09 17:36:42 +0200

    profile name is added

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 const Profile = () => {
   const location = useLocation();
   console.log(location.state);
-  return <div>We are in the profile page</div>;
+  return <div className=" ">welcome {location.state.first_name}</div>;
 };
 
 export default Profile;
